0340dbe8ece55e7513c79dc876490f2db010c72f
Timothy Farrar <timothyfarrar@sosensible.com>
2013-12-24 10:47:04 -0800

    Added editor to Editor pane

--- subapps/editor/index.js
+++ subapps/editor/index.js
@@ -1,3 +1,8 @@
 $(function(){
-    
+    var myCodeMirror = CodeMirror(document.body, {
+		lineNumbers: true,
+		value: "function myScript(){return 100;}\n",
+  		mode:  "javascript"
+	});
+    console.log(myCodeMirror);
 })
